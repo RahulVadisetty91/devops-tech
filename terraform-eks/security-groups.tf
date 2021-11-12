ea1e945d9729e5214767ffc00553ae2070139597
Azmat Hasan <azmathasan@Azmats-MacBook-Pro.local>
2021-11-12 17:30:50 +0530

    Added prometheous stack helm chart

--- terraform-eks/security-groups.tf
+++ terraform-eks/security-groups.tf
@@ -3,12 +3,12 @@ resource "aws_security_group" "worker_group" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    from_port = 22
-    to_port   = 22
+    from_port = 0
+    to_port   = 65535
     protocol  = "tcp"
 
     cidr_blocks = [
-      "10.0.0.0/8",
+      "0.0.0.0/0",
     ]
   }
 }
